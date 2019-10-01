daad2065cbffd381fa380019676ad1d22a461567
Regina00at <55528423+Regina00at@users.noreply.github.com>
2019-10-01 20:22:31 +0900

    Update 2D pattern.js

--- proj.3/2D pattern.js
+++ proj.3/2D pattern.js
@@ -1,9 +1,14 @@
+let img, img1, img2;
+function preload(){
+  img=loadImage('earth.png');
+  img1=loadImage('planet1.png');
+  img2=loadImage('planet3.png');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   noStroke();
-  
-    
 }
 
 function draw() {
@@ -14,12 +19,16 @@ function draw() {
   var backcolor = map(mouseX, 0, windowWidth, -30, 70)
   
   background(39+backcolor, 34+backcolor, 51+backcolor);
+  image(img1, windowWidth*0.2, windowHeight*0.5, 100, 100);
+  image(img, windowWidth*0.45, windowHeight*0.5, 100, 100);
+  image(img2, windowWidth*0.7, windowHeight*0.5, 100, 100);
   randomSeed(0);
   for(y=0;y<windowHeight;y+=40){
     for(x=0;x<windowWidth;x+=40){
       noStroke();
       fill(250,190,40);
       ellipse(x,y,3,3);}}
+  drawstar
   
 
   
@@ -40,7 +49,7 @@ function drawstar(x,y){
   fill(random(235,255), random(180,200), random(30,50));  
   translate(x, y);
   rotate(frameCount / -100.0);
-star(random(-50,50),random(-30,40), random(15,20), random(3,7), random(3,7));
+  star(random(-50,50),random(-30,40), random(15,20), random(3,7), random(3,7));
   pop();
 }
 
